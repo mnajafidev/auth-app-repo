32446d39afd21ffd9f8d4b53750ac4b00086f2d0
m.najafi <m.najafi.dev@gmail.com>
2025-05-23 18:27:47 -0400

    first commit

---
+++ src/test/java/ir/home/project/authapp/AuthappApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.home.project.authapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
